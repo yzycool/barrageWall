c6ed09410099f8d9ee491d0b0b99c25ac36a0926
2596915059@qq.com <2596915059@qq.com>
2024-04-02 17:03:37 +0800

    feat: 基本功能完成，待样式优化

--- index.js
+++ index.js
@@ -1,47 +1,93 @@
-function sendDanmu() {
-  var input = document.getElementById('danmuInput')
-  var message = input.value.trim()
-
-  if (message !== '') {
-    var danmuContainer = document.getElementById('danmuContainer')
-    var danmu = document.createElement('div')
-    danmu.className = 'danmu'
-    danmu.textContent = message
-    danmu.style.fontSize = randomFontSize() + 'px'
-    danmu.style.top = randomPosition(0, danmuContainer.offsetHeight - 30) + 'px'
-    danmu.style.color = randomFontColor()
-    danmuContainer.appendChild(danmu)
-
-    // 随机速度
-    var speed = randomSpeed()
-    danmu.style.animationDuration = speed + 's'
-
-    //每隔一段时间发送过期的弹幕
-    setTimeout(function () {
-      danmuContainer.appendChild(danmu)
-    }, speed * 1000) // 根据速度设置定时器
-
-    input.value = ''
+const switchBtn = document.getElementById('switch')
+const sendBtn = document.getElementById('send')
+const clearBtn = document.getElementById('clear')
+var bulletDomList = document.getElementsByClassName('bullet')
+const containerDOM = document.getElementById('screenContainer')
+//弹幕定时器列表
+let timers = []
+//弹幕显示控制
+let isShow = true
+//用户点击按钮处理
+function addBullet() {
+  const textDom = document.getElementById('screenBulletText')
+  const inputVal = textDom.value
+  if (!inputVal) {
+    alert('请输入弹幕内容')
+    return
   }
+  addInterval(createScreenBullet(inputVal))
+  textDom.value = ''
 }
-
-function randomPosition(min, max) {
+//用户输入后创建弹幕
+function createScreenBullet(text) {
+  let bulletDom = document.createElement('div')
+  bulletDom.innerHTML = text
+  bulletDom.className = 'bullet'
+  const fontColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')'
+  const fontSize = getRandomInteger(12, 36) + 'px'
+  const left = containerDOM.offsetWidth + 'px'
+  let top = Math.floor(Math.random() * 400)
+  top = top > 302 ? '302px' : `${top}px`
+  bulletDom.style.cssText = `
+          position: absolute;
+          color: ${fontColor};
+          font-size: ${fontSize};
+          left: ${left};
+          top: ${top};
+      `
+  containerDOM.append(bulletDom)
+  return bulletDom
+}
+//添加定时器，利用定时器的频率控制速度，缺点是弹幕过多极其耗内存
+function addInterval(intervalDom) {
+  let left = intervalDom.offsetLeft
+  const timer = setInterval(function () {
+    left--
+    intervalDom.style.left = `${left}px`
+    if (intervalDom.offsetLeft + intervalDom.offsetWidth < 0) {
+      intervalDom.style.left = containerDOM.offsetWidth + 'px'
+      left = intervalDom.offsetLeft
+    }
+  }, getRandomInteger(10, 40)) //区间10-40最佳
+  timers.push(timer)
+}
+//弹幕开关
+switchBtn.onclick = function () {
+  if (isShow) {
+    for (let i = 0; i < bulletDomList.length; i++) {
+      bulletDomList[i].style.opacity = 0
+    }
+    isShow = false
+  } else {
+    for (let i = 0; i < bulletDomList.length; i++) {
+      bulletDomList[i].style.opacity = 1
+    }
+    isShow = true
+  }
+  switchBtn.innerText = isShow ? '关闭弹幕' : '打开弹幕'
+}
+//清除所有弹幕
+clearBtn.onclick = function () {
+  while (containerDOM.firstChild) {
+    containerDOM.removeChild(containerDOM.firstChild)
+  }
+  for (let i = 0; i < timers.length; i++) {
+    clearInterval(timers[i])
+  }
+  timers = []
+}
+// 生成区间 [min, max] 内的随机整数
+function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function randomFontColor() {
-  var r = Math.floor(Math.random() * 254)
-  var g = Math.floor(Math.random() * 254)
-  var b = Math.floor(Math.random() * 254)
-  return 'rgb(' + r + ',' + g + ',' + b + ')'
+sendBtn.onclick = function () {
+  addBullet()
 }
-function randomFontSize() {
-  return Math.floor(Math.random() * 25) + 12 // 字体大小在12到36之间
-}
-function randomSpeed() {
-  return Math.random() * 10 + 5 // 速度在5到15之间
-}
-function clearDanmu() {
-  var danmuContainer = document.getElementById('danmuContainer')
-  danmuContainer.innerHTML = '' // 清除所有弹幕
+
+document.onkeyup = function (e) {
+  var code = e.charCode || e.keyCode
+  if (code === 13) {
+    addBullet()
+  }
 }
